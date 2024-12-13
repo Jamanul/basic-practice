fbfc64dd4790722debc47ea6b97e4a0850430ce1
Jamanul <jk.skb.25@gmail.com>
2024-12-13 21:19:05 +0600

    leap year

--- practice.js
+++ practice.js
@@ -1,43 +1,57 @@
+// const number =[1,2,3,4,5,6,7,8]
+
+// const isEven =()=>{
+//   number.forEach((num)=>{
+//     if(num %2 == 0){
+//         console.log("even",num)
+//     }   
+//     else{
+//         console.log('odd',num)
+//     }
+//   })
+// }
+
+// //isEven()
+
+// const addArray =()=>{
+//     let total =0;
+//     for(const num of number) {
+//         total += num ;
+//     }
+//     return total
+// }
+
+// console.log(addArray())
+
+// const isLeapYear =(year)=>{
+//     if(year % 100 !=0){
+//          if(year %4 ==0){
+//         console.log('the year is leap year')
+//     }
+//     else {
+//         console.log('the year is not a leap year')
+//     }
+//     }
+//     if(year %400 ==0){
+//         console.log('the year is leap year')
+//     }
+//     if(year % 100 ==0 && year %400 != 0){
+//         console.log('the year is not leap year')
+//     }
+// } 
+
+// isLeapYear(1600)
+
+
 const number =[1,2,3,4,5,6,7,8]
 
-const isEven =()=>{
-  number.forEach((num)=>{
-    if(num %2 == 0){
-        console.log("even",num)
-    }   
-    else{
-        console.log('odd',num)
+const oddAvg = ()=>{
+    let total =0
+    for(i=0;i<number.length;i++){
+        if(i %2 !=0){
+            total += num 
+        }
     }
-  })
 }
 
-//isEven()
-
-const addArray =()=>{
-    let total =0;
-    for(const num of number) {
-        total += num ;
-    }
-    return total
-}
-
-console.log(addArray())
-
-const isLeapYear =(year)=>{
-    if(year % 100 !=0){
-         if(year %4 ==0){
-        console.log('the year is leap year')
-    }
-    else {
-        console.log('the year is not a leap year')
-    }
-    }
-    if(year %400 ==0){
-        console.log('the year is leap year')
-    }
-    if(year % 100 ==0 && year %400 != 0){
-        console.log('the year is not leap year')
-    }
-} 
-
-isLeapYear(1600)
+console.log(oddAvg())
